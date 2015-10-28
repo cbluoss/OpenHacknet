d0b5d9e7ec876bb5c643bcab1c1342bc2b4ef00b
afffsdd <afffsdd@users.noreply.github.com>
2015-10-27 23:04:18 -0400

    Remove file header

--- MedicalPortExe.cs
+++ MedicalPortExe.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Hacknet.MedicalPortExe
-// Assembly: Hacknet, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 48C62A5D-184B-4610-A7EA-84B38D090891
-// Assembly location: C:\Program Files (x86)\Steam\SteamApps\common\Hacknet\Hacknet.exe
-
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 
 namespace Hacknet
 {
--- PlayerBonuses.cs
+++ PlayerBonuses.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Hacknet.PlayerBonuses
-// Assembly: Hacknet, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 48C62A5D-184B-4610-A7EA-84B38D090891
-// Assembly location: C:\Program Files (x86)\Steam\SteamApps\common\Hacknet\Hacknet.exe
-
-namespace Hacknet
+﻿namespace Hacknet
 {
     public struct PlayerBonuses
     {
